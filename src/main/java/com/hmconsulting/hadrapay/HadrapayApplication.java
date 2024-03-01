75f8d92aa02c2a6d856a9371ec88402b369f428f
Hadra 92i <hadraiizi547@gmail.com>
2024-03-01 17:57:33 +0000

    first commit

---
+++ src/main/java/com/hmconsulting/hadrapay/HadrapayApplication.java
@@ -0,0 +1,13 @@
+package com.hmconsulting.hadrapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadrapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadrapayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmconsulting/hadrapay/HadrapayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmconsulting.hadrapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadrapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
